e9ae3239e4f53d33c6ccc2b8a19e5e6f3e9b14d3
Adnan Gondal <adnangondal1@hotmail.com>
2022-01-18 17:08:32 +0000

    Adnan done

--- server/app/com/example/onlineshoppingcart/controllers/ApiController.scala
+++ server/app/com/example/onlineshoppingcart/controllers/ApiController.scala
@@ -1,14 +1,14 @@
 package com.example.onlineshoppingcart.controllers
 
 import com.google.inject.{Inject, Singleton}
-import dao.ProductsDao
+import dao.{CartsDao, ProductsDao}
 import io.circe.generic.auto._
 import io.circe.syntax._
 import io.circe.parser.decode
 import io.circe.syntax.EncoderOps
 import play.api.libs.circe.Circe
-import play.api.mvc.{AbstractController, BaseController, ControllerComponents}
-import com.example.onlineshoppingcart.shared.Product
+import play.api.mvc.{AbstractController, BaseController, ControllerComponents, Result}
+import com.example.onlineshoppingcart.shared.{Cart, Product, ProductInCart}
 import play.api.Logger
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -17,14 +17,77 @@ import scala.concurrent.Future
 
 @Singleton
 class ApiController @Inject()(cc: ControllerComponents, productsDao:
-ProductsDao) extends AbstractController(cc) with BaseController with Circe {
+ProductsDao, cartsDao: CartsDao) extends AbstractController(cc) with BaseController with Circe {
+
+  val recoverError: PartialFunction[Throwable, Result] = {
+    case e => {
+      logger.error("Database writing error",e)
+      InternalServerError("Cannot write in the database")
+    }
+  }
+
+  def login() = Action { request =>
+    request.body.asText match {
+      case None => BadRequest
+      case Some(user) => Ok.withSession("user" -> user)
+    }
+  }
   // *********** CART Controler ******** //
-//  def listCartProducts() = ???
-//  def deleteCartProduct(id: String) = ???
-//  def addCartProduct(id: String, quantity: String) =
-//    ???
-//  def updateCartProduct(id: String, quantity: String) =
-//    ???
+ def listCartProducts() = Action.async { req =>
+   val userOption = req.session.get("user")
+
+   userOption match {
+     case Some(user) => {
+       logger.info(s"User '$user' is asking for the list of product in the cart")
+       val futureInsert = cartsDao.cart4(user)
+
+       futureInsert.map(products => {
+         Ok(products.asJson)
+       }).recover(recoverError)
+     }
+     case None => Future.successful(Unauthorized)
+   }
+ }
+  def deleteCartProduct(id: String) = Action.async {request =>
+    val userOption = request.session.get("user")
+    userOption match {
+      case Some(user) => {
+        logger.info(s"User '$user' is asking to delete the product'$id' from the cart")
+        val futureInsert = cartsDao.remove(ProductInCart(user, id))
+        futureInsert.map(_ => Ok).recover(recoverError)
+      }
+      case None => Future.successful(Unauthorized)
+    }
+  }
+
+  def addCartProduct(id: String, quantity: String) = Action.async{ req =>
+    val userOption = req.session.get("user")
+
+    userOption match{
+      case Some(user) => {
+        val futureInsert = cartsDao.insert(Cart(user,id,quantity.toInt))
+        futureInsert.map(_ => Ok).recover(recoverError)
+      }
+      case None => Future.successful(Unauthorized)
+    }
+
+  }
+
+
+  def updateCartProduct(id: String, quantity: String) = Action.async { request =>
+      val userOption = request.session.get("user")
+    print("we are hereee...."+userOption)
+      userOption match {
+        case Some(user) => {
+          logger.info(s"User '$user' is updating the product'$id' in it is cart with a quantity of '$quantity")
+          val futureInsert = cartsDao.update(Cart(user, id,
+            quantity.toInt))
+          futureInsert.map(_ => Ok).recover(recoverError)
+        }
+        case None => Future.successful(Unauthorized)
+      }
+  }
+
   // *********** Product Controler ******** //
   def listProduct() = Action.async { req =>
     val futureProducts = productsDao.all()
--- server/test/APISpec.scala
+++ server/test/APISpec.scala
@@ -1,17 +1,23 @@
 
+import com.example.onlineshoppingcart.shared.Cart
 import org.scalatest.concurrent.ScalaFutures
 import org.scalatest.time.{Millis, Seconds, Span}
 import org.scalatestplus.play.PlaySpec
 import org.scalatestplus.play.guice.GuiceOneServerPerSuite
 import play.api.http.Status.OK
-import play.api.libs.ws.WSClient
+import play.api.libs.ws.{DefaultWSCookie, WSClient}
+import io.circe.parser.decode
+import io.circe.generic.auto._
+import io.circe.syntax._
 
 import scala.concurrent.duration._
 import scala.concurrent.Await
 import scala.language.postfixOps
 
 class APISpec extends PlaySpec with ScalaFutures with GuiceOneServerPerSuite{
+
   val baseURL = s"localhost:$port/api"
+  val login = s"http://$baseURL/session"
   val productsURL = s"http://$baseURL/products"
   val addProductURL = s"http://$baseURL/product"
   val productsInCartURL = s"http://$baseURL/cart/products"
@@ -20,10 +26,21 @@ class APISpec extends PlaySpec with ScalaFutures with GuiceOneServerPerSuite{
   def actionProductInCartURL(productID: String, quantity: Int) =
     s"http://$baseURL/cart/products/$productID/quantity/$quantity"
   implicit val defaultPatience: PatienceConfig = PatienceConfig(timeout = Span(10, Seconds), interval = Span(1000, Millis))
+  implicit val ec: scala.concurrent.ExecutionContext = scala.concurrent.ExecutionContext.global
 
   "The API" should {
 
       val wsClient = app.injector.instanceOf[WSClient]
+
+    lazy val defaultCookie = {
+      val loginCookies = Await.result(wsClient.url(login).post("me").map(p=>{
+        p.headers.get("Set-Cookie").map(_.head.split(";").head)
+      }),1 seconds)
+
+      val play_session = loginCookies.get.split("=").tail.mkString("")
+      DefaultWSCookie("PLAY_SESSION", play_session)
+    }
+
 
     "list all the product" in {
 
@@ -45,37 +62,79 @@ class APISpec extends PlaySpec with ScalaFutures with GuiceOneServerPerSuite{
       response.body must include("NewOne")
     }
 
+    "list all the products in a cart" in {
+      // login builds session cookie
+
+
+      val response = (wsClient.url(productsInCartURL).addCookies(defaultCookie)).get().futureValue
+
+      //println(response)
+      response.status mustBe OK
+
+      println(response.body)
+      val listOfProduct = decode[Seq[Cart]](response.body)
+      //println(listOfProduct.right.get)
+      listOfProduct.getOrElse(Seq.empty) mustBe empty
+
+    }
+
     "add a product in the cart" in {
       val productID = "ALD1"
       val quantity = 1
-      val posted = wsClient.url(actionProductInCartURL(productID,quantity)).post("").futureValue
+      val posted = wsClient.url(actionProductInCartURL(productID,quantity)).addCookies(defaultCookie).post("").futureValue
       posted.status mustBe OK
+
+      val response = wsClient.url(productsInCartURL).addCookies(defaultCookie).get().futureValue
+      println(response)
+      response.status mustBe OK
+      response.body must include("ALD1")
+
     }
 
     "delete a product from the cart" in {
       val productID = "ALD1"
       val quantity = 1
       val posted = wsClient.url(deleteProductInCartURL(productID))
-        .delete().futureValue
+        .addCookies(defaultCookie).delete().futureValue
       posted.status mustBe OK
+
+      val response = wsClient.url(productsInCartURL).addCookies(defaultCookie).get().futureValue
+      println(response)
+      response.status mustBe OK
+      response.body mustNot include ("ALD1")
     }
 
     "Update a product quantity in the cart" in {
 
         val productID = "ALD1"
         val quantity = 1
-        val posted = wsClient.url(actionProductInCartURL(productID,
-          quantity))
-          .post("").futureValue
+        val posted = wsClient.url(actionProductInCartURL(productID, quantity)).addCookies(defaultCookie).post("").futureValue
 
         posted.status mustBe OK
 
         val newQuantity = 99
         val update = wsClient.url(actionProductInCartURL(productID,
-          newQuantity)).put("").futureValue
+          newQuantity)).addCookies(defaultCookie).put("").futureValue
         update.status mustBe OK
 
+      val response = wsClient.url(productsInCartURL).addCookies(defaultCookie).get().futureValue
+      println(response)
+      response.status mustBe OK
+      response.body must include(productID)
+      response.body must include(newQuantity.toString)
+    }
 
+    "return a cookie when a user logs in" in {
+      val cookieFuture = wsClient.url(login).post("myID").map {
+        response => {
+          response.headers.get("Set-Cookie").map(
+            header => header.head.split(";")
+              .filter(_.startsWith("PLAY_SESSION")).head)
+        }
+      }
+      val loginCookies = Await.result(cookieFuture,1 second)
+      val play_session_key =  loginCookies.get.split("=").head
+      play_session_key must equal("PLAY_SESSION")
     }
 
 
